8a8189e3459523143a212d375d702022ecd5943d
Kalysh <503292@ukr.net>
2019-04-04 13:32:32 +0300

    setting for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,16 @@ mvn clean package
 
 echo 'Copy files ...'
 
-scp -i ~/.ssh/id_rsa       \
-    target/twitt-1.0-SNAPSHOT.jar
+scp -i ~/.ssh/id_rsa \
+    target/twitt-1.0-SNAPSHOT.jar \
+    dru@51.77.152.216:/home/dru/
+
+echo 'Restart server ...'
+
+ssh -i ~/.ssh/id_rsa dru@51.77.152.216 <<EOF
+
+pgrep java | xargs kill -9
+nohup java -jar twitt-1.0-SNAPSHOT.jar > log.txt &
+EOF
+
+echo 'Bye'
